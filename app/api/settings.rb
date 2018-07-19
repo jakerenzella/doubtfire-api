d22f2ce5d8fc5b1eea60ee57815348fd24f10dbd
Jake Renzella <j@kerenzella.com>
2018-07-19 15:31:27 +1000

    NEW: Add new teaching periods unit endpoint

--- app/api/settings.rb
+++ app/api/settings.rb
@@ -13,5 +13,23 @@ module Api
         externalName: Doubtfire::Application.config.institution[:product_name]
       }
     end
+
+    #
+    # Returns the current units with teaching periods
+    # Return:
+    # Unit Name, unit ID, teaching period name, year
+    #
+    desc 'Return all the units teaching period information'
+    get '/units/teaching_periods' do
+      units_with_teaching_periods = Unit.where('teaching_period_id is not NULL').select([:id, :name, :code, :teaching_period_id])
+      units_with_teaching_periods.map do |unit|
+        {
+          unit_id: unit.id,
+          unit_code: unit.code,
+          period_name: TeachingPeriod.find(unit.teaching_period_id).period
+        }
+      end
+    end
+
   end
 end
